6187ab4ed65c38694bd6e4380f7ca9c24fa293eb
vgostovic <vukasin4918@iths.edu.rs>
2022-06-01 23:06:04 +0200

    a

--- db_init_script.sql
+++ db_init_script.sql
@@ -1,25 +1,31 @@
-CREATE TABLE racun (
+drop table racun;
+drop table stavka;
+drop table proizvod;
+
+CREATE TABLE proizvod (
     id int NOT NULL AUTO_INCREMENT,
-    br_racuna int,
-    stavka int,
-    datum date,
-    primary key (id),
-    FOREIGN KEY (stavka) REFERENCES stavka(id)
+    proizvodjac varchar(255) NOT NULL,
+    naziv varchar(255) NOT NULL,
+    cena int NOT NULL,
+    primary key (id)
 );
 CREATE TABLE stavka (
     id int NOT NULL AUTO_INCREMENT,
-    proizvod int,
-    kolicina int,
+    proizvod int NOT NULL,
+    kolicina int NOT NULL,
     primary key (id),
     FOREIGN KEY (proizvod) REFERENCES proizvod(id)
 );
-CREATE TABLE proizvod (
+CREATE TABLE racun (
     id int NOT NULL AUTO_INCREMENT,
-    proizvodjac varchar(255),
-    naziv varchar(255),
-    cena int,
-    primary key (id)
+    br_racuna int NOT NULL,
+    stavka int NOT NULL,
+    datum date NOT NULL,
+    primary key (id),
+    FOREIGN KEY (stavka) REFERENCES stavka(id)
 );
+
+
 
 INSERT INTO proizvod (proizvodjac, naziv, cena)
 VALUES ('veselin', 'prevara', 213);
@@ -27,9 +33,18 @@ VALUES ('veselin', 'prevara', 213);
 INSERT INTO proizvod (proizvodjac, naziv, cena)
 VALUES ('veselin', 'visa prevara', 213);
 
-INSERT INTO stavka (proizvod, kolicina)
-VALUES ( 1, 1);
+INSERT INTO proizvod (proizvodjac, naziv, cena)
+VALUES ('JOVAN', 'OOOOO', 213);
 
-INSERT INTO racun (br_racuna, stavka)
-VALUES ( 1, 1);
+INSERT INTO proizvod (proizvodjac, naziv, cena)
+VALUES ('JOVAN', 'EEE', 213);
+
+INSERT INTO proizvod (proizvodjac, naziv, cena)
+VALUES ('JOVAN', 'III', 213);
+
+INSERT INTO stavka (proizvod, kolicina)
+VALUES (1, 1);
+
+INSERT INTO racun (br_racuna, stavka, datum)
+VALUES ( 1, 1, "2017-06-15");
 
